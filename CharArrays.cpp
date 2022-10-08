e2844e842bb3d03cfd7ead31c9066b33d4237050
khaledrefaat <khaledrefaat08@gmail.com>
2022-10-08 10:50:03 +0200

    solving medium challenges

--- CharArrays.cpp
+++ CharArrays.cpp
@@ -127,3 +127,79 @@ using namespace std;
 //
 //    return 0;
 //}
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////
+// // medium challenges
+
+
+// ////////////////////////////////////////////
+// // Problem #1: Compressing
+//int main() {
+//    string str;
+//    cin >> str;
+//
+//    int counter = 1;
+//    for (int i = 1; i < str.size(); i++) {
+//
+//        if (str[i] != str[i - 1]) {
+//            cout << str[i - 1] << counter << "_";
+//            counter = 1;
+//        } else if (i == str.size() - 1) cout << str[i] << counter + 1;
+//        else counter++;
+//    }
+//}
+//
+
+
+// ////////////////////////////////////////////
+// // Problem #2: Compare strings
+// // Read 2 strings, then output Yes if the first string smaller than or equal to second string. otherwise, output No
+// // Don't use < operator to compare strings. use loops
+//int main() {
+//    string str1, str2;
+//    cin >> str1 >> str2;
+//
+//    bool is_str2_smaller = true;
+//    for (int i = 0; i < str2.size(); i++) {
+//        if (!str1[i]) is_str2_smaller = false;
+//    }
+//
+//    if (is_str2_smaller) cout << "YES";
+//    else cout << "NO";
+//
+//    return 0;
+//}
+
+
+// ////////////////////////////////////////////
+// // Problem #3: Add 5555
+//int main() {
+//    string num;
+//
+//    // Guarantee input: must be 6 digits or more
+//    cin >> num;
+//
+//    int sz = num.size();
+//
+//    int carry = 0;
+//    for (int i = 0; i < sz; ++i) {
+//        int digit = num[sz - 1 - i] - '0';
+//
+//        if (i < 4)
+//            digit += 5;			// add 5 in first 4 times
+//        digit += carry;		// add any carry
+//
+//        if (digit >= 10)
+//            digit -= 10, carry = 1;
+//        else
+//            carry = 0;
+//
+//        num[sz - 1 - i] = digit + '0';
+//    }
+//    if(carry)
+//        cout<<1;
+//    cout << num;
+//
+//    return 0;
+//}
